c091857f1d0e93e2b8b2e8c665514f727695ecf9
flipfrog <>
2020-01-24 10:35:08 +0900

    divide into app and framework (still be in-progress)

--- refactored/FrameEngine.js
+++ refactored/FrameEngine.js
@@ -1,47 +1,30 @@
 export default class {
     // set canvas
+    canvas = null;
+    ctx = null;
     setCanvas(canvasId) {
         // set canvas and context
         this.canvas = document.getElementById(canvasId);
         this.ctx = this.canvas.getContext('2d');
-        // register key event procedure
-        this.canvas.addEventListener('keydown', e => this.procKeyEvents(e, this.KEY_KEYDOWN), false);
-        this.canvas.addEventListener('keyup', e => this.procKeyEvents(e, this.KEY_KEYUP), false);
-        this.canvas.addEventListener('click', e => this.procMouseClick(e), false);
+    }
+
+    getCanvas = () => this.canvas;
+    getCtx = () => this.ctx;
+
+    // set frame updating handler
+    updateHandler = () => {};
+    setUpdateHandler(handler) {
+        this.updateHandler = handler;
+    }
+
+    // set event handler
+    setEventHandler(eventName, handler) {
+        this.canvas.addEventListener(eventName, e => handler(e, eventName), false);
     }
 
     // start updating canvas frame
     startFrame () {
         requestAnimationFrame(this.drawFrame.bind(this));
-    }
-
-    // process mouse click
-    procMouseClick(e) {
-        const rect = e.target.getBoundingClientRect();
-        this.x = e.clientX - rect.left;
-        this.y = e.clientY - rect.top;
-    }
-
-    // process key events
-    KEY_KEYDOWN = 'keydown';
-    KEY_KEYUP = 'keyup';
-    KEY_LEFT = 'ArrowLeft';
-    KEY_RIGHT = 'ArrowRight';
-    KEY_UP = 'ArrowUp';
-    KEY_DOWN = 'ArrowDown';
-    KEY_SPACE = 'Space';
-    KEY_ARROW_KEYS = [this.KEY_LEFT, this.KEY_RIGHT, this.KEY_UP, this.KEY_DOWN];
-    arrowKeyDownStatuses = {ArrowLeft: false, ArrowRight: false, ArrowUp: false, ArrowDown: false};
-    spaceKeyDownStatus = false;
-    procKeyEvents(e, handleType) {
-        // check arrow keys
-        if (this.KEY_ARROW_KEYS.indexOf(e.key) >= 0) {
-            this.arrowKeyDownStatuses[e.key] = (handleType === this.KEY_KEYDOWN);
-        }
-        // check space key
-        if (e.key === this.KEY_SPACE) {
-            this.spaceKeyDownStatus = (handleType === this.KEY_KEYDOWN);
-        }
     }
 
     // create texture atlas data structure from control file(js file)
@@ -63,14 +46,17 @@ export default class {
     }
 
     // draw sprite on canvas
-    drawSprite(ctx, sprite, x, y) {
-        ctx.save();
-        ctx.translate(x, y);
-        ctx.rotate(sprite.rotate * Math.PI/180);
-        ctx.drawImage(sprite.image,
-            sprite.x, sprite.y, sprite.width, sprite.height,
-            -sprite.anchor.x, -sprite.anchor.y, sprite.width, sprite.height);
-        ctx.restore();
+    drawSprite(ctx, spriteName, x, y) {
+        const sprite = this.spriteMap[spriteName] || null;
+        if (sprite) {
+            ctx.save();
+            ctx.translate(x, y);
+            ctx.rotate(sprite.rotate * Math.PI/180);
+            ctx.drawImage(sprite.image,
+                sprite.x, sprite.y, sprite.width, sprite.height,
+                -sprite.anchor.x, -sprite.anchor.y, sprite.width, sprite.height);
+            ctx.restore();
+        }
     }
 
     // load image files
@@ -98,30 +84,7 @@ export default class {
         const currentMilliSec = currentTime/1000;
         const delta = this.lastMilliSec ? currentMilliSec - this.lastMilliSec : 0;
         this.lastMilliSec = currentMilliSec;
-        this.update(delta);
+        this.updateHandler(delta);
         requestAnimationFrame(this.drawFrame.bind(this));
-    }
-
-    // update canvas
-    x = 50;
-    y = 50;
-    pps = 20; // speed px/sec
-    update(delta) {
-        // move sprite according to keydown status of arrow keys.
-        const pxDelta = delta * this.pps;
-        if (this.arrowKeyDownStatuses[this.KEY_LEFT]) {
-            this.x = (this.x - pxDelta) % this.canvas.width;
-        }
-        if (this.arrowKeyDownStatuses[this.KEY_RIGHT]) {
-            this.x = (this.x + pxDelta) % this.canvas.width;
-        }
-        if (this.arrowKeyDownStatuses[this.KEY_UP]) {
-            this.y = (this.y - pxDelta) % this.canvas.height;
-        }
-        if (this.arrowKeyDownStatuses[this.KEY_DOWN]) {
-            this.y = (this.y + pxDelta) % this.canvas.height;
-        }
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.drawSprite(this.ctx, this.spriteMap['pengo_2.png'], this.x, this.y);
     }
 };
--- refactored/main.js
+++ refactored/main.js
@@ -4,6 +4,64 @@ import atlas from './atlas.js';
 (function(){
     const engine = new FrameEngine();
     engine.setCanvas('canvas_1');
+    engine.setUpdateHandler(update);
+    // register key event procedure
+    engine.setEventHandler('keydown', procKeyEvents);
+    engine.setEventHandler('keyup', procKeyEvents);
+    engine.setEventHandler('click', procMouseClick);
+
     engine.createTextureAtlas(atlas)
         .then(() => engine.startFrame());
+
+    // update canvas
+    let x = 50;
+    let y = 50;
+    const pps = 20; // speed px/sec
+    function update(delta) {
+        const ctx = engine.getCtx();
+        const canvas = engine.getCanvas();
+        // move sprite according to keydown status of arrow keys.
+        const pxDelta = delta * pps;
+        if (arrowKeyDownStatuses[KEY_LEFT]) {
+            x = (x - pxDelta) % canvas.width;
+        }
+        if (arrowKeyDownStatuses[KEY_RIGHT]) {
+            x = (x + pxDelta) % canvas.width;
+        }
+        if (arrowKeyDownStatuses[KEY_UP]) {
+            y = (y - pxDelta) % canvas.height;
+        }
+        if (arrowKeyDownStatuses[KEY_DOWN]) {
+            y = (y + pxDelta) % canvas.height;
+        }
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        engine.drawSprite(ctx, 'pengo_2.png', x, y);
+    }
+
+    // process key events
+    const KEY_LEFT = 'ArrowLeft';
+    const KEY_RIGHT = 'ArrowRight';
+    const KEY_UP = 'ArrowUp';
+    const KEY_DOWN = 'ArrowDown';
+    const KEY_SPACE = 'Space';
+    const KEY_ARROW_KEYS = [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN];
+    const arrowKeyDownStatuses = {ArrowLeft: false, ArrowRight: false, ArrowUp: false, ArrowDown: false};
+    let spaceKeyDownStatus = false;
+    function procKeyEvents(e, handleType) {
+        // check arrow keys
+        if (KEY_ARROW_KEYS.indexOf(e.key) >= 0) {
+            arrowKeyDownStatuses[e.key] = (handleType === 'keydown');
+        }
+        // check space key
+        if (e.key === KEY_SPACE) {
+            spaceKeyDownStatus = (handleType === 'keydown');
+        }
+    }
+
+    // process mouse click
+    function procMouseClick(e) {
+        const rect = e.target.getBoundingClientRect();
+        x = e.clientX - rect.left;
+        y = e.clientY - rect.top;
+    }
 })();
