ee077af9f765f54782eccf027fa41c8c6a1be98c
flipfrog <>
2020-01-29 16:59:59 +0900

    add particle api(in-progress)

---
+++ particleSystem/FrameEngine.js
@@ -0,0 +1,285 @@
+export default class {
+    // literals
+    // key symbols for event handler
+    KEY_SYMBOL_LEFT = 'ArrowLeft';
+    KEY_SYMBOL_RIGHT = 'ArrowRight';
+    KEY_SYMBOL_UP = 'ArrowUp';
+    KEY_SYMBOL_DOWN = 'ArrowDown';
+    KEY_SYMBOL_SPACE = 'Space';
+
+    // display fps
+    displayFps = false;
+
+    // data
+    clientData = {};
+
+    // canvas as backing store data
+    /** @type HTMLCanvasElement */
+    offScreenCanvas = null;
+    offScreenCtx = null;
+    // set canvas
+    /** @type HTMLCanvasElement */
+    canvas = null;
+    /** @type CanvasRenderingContext2D */
+    ctx = null;
+    setCanvas(canvasId, useOffScreenCanvas=false) {
+        // set canvas and context
+        this.canvas = document.getElementById(canvasId);
+        this.ctx = this.canvas.getContext('2d');
+        if (useOffScreenCanvas) {
+            this.offScreenCanvas = document.createElement('canvas');
+            this.offScreenCanvas.width = this.canvas.width;
+            this.offScreenCanvas.height = this.canvas.height;
+            this.offScreenCtx = this.offScreenCanvas.getContext('2d');
+        }
+    }
+
+    // set client data
+    setClientData(data) {
+        this.clientData = Object.assign({}, data);
+    }
+
+    getClientData = () => this.clientData;
+    getCanvas = () => (this.offScreenCanvas ? this.offScreenCanvas : this.canvas);
+    getCtx = () => (this.offScreenCtx ? this.offScreenCtx : this.ctx);
+    setDisplayFps = (displayFps) => {this.displayFps = displayFps};
+
+    // set user-update handler
+    updateHandler = () => {};
+    setUpdateHandler(handler) {
+        this.updateHandler = handler;
+    }
+
+    // add sprite to array
+    sprites = {};
+    spriteMap = {};
+    addSprite(sprite) {
+        if (!this.sprites[sprite.layerNo]) {
+            this.sprites[sprite.layerNo] = {};
+        }
+        if(!this.sprites[sprite.layerNo][sprite.tag] && !this.spriteMap[sprite.tag]) {
+            this.sprites[sprite.layerNo][sprite.tag] = sprite;
+            this.spriteMap[sprite.tag] = sprite;
+        } else {
+            console.log(`sprite tag name ${sprite.tag} already exits`);
+        }
+    }
+
+    // remove sprite
+    removeSprite(tag) {
+        const layerNos = Object.keys(this.sprites);
+        let emptyLayerNo = -1;
+        layerNos.forEach(layerNo => {
+            if (this.sprites[layerNo][tag] && this.spriteMap[tag]) {
+                delete this.sprites[layerNo][tag];
+                delete this.spriteMap[tag];
+            } else {
+                console.log(`sprite tag name ${tag} does not exist`);
+            }
+            if (this.sprites[layerNo].length === 0) {
+                emptyLayerNo = layerNo;
+            }
+        });
+        if (emptyLayerNo >= 0) {
+            delete this.sprites[emptyLayerNo];
+        }
+    }
+
+    // get sprite
+    getSprite(tag) {
+        return this.spriteMap[tag] || null;
+    }
+
+    // set event handler
+    EVENT_TYPE_KEYDOWN = 'keydown';
+    EVENT_TYPE_KEYUP = 'keyup';
+    EVENT_TYPE_CLICK = 'click';
+    setEventHandler(type, handler) {
+        this.canvas.addEventListener(type, e => {
+            handler(this, e, type);
+            e.preventDefault();
+        }, false);
+    }
+
+    // start updating canvas frame
+    startFrame () {
+        requestAnimationFrame(this.drawFrame.bind(this));
+    }
+
+    // create texture atlas data structure from control file(js file)
+    // TODO: add textures' border-area
+    textureMap = {};
+    async loadTextureAtlas(atlas) {
+        const imageFiles = atlas.map(file => file.imageFile);
+        // load image for textures
+        const images = await this.loadImages(imageFiles);
+        // analyze texture atlas structure
+        atlas.forEach((file, index) => {
+            file.textures.forEach(texture => {
+                texture.image = images[index];
+                this.textureMap[texture.name] = texture;
+            });
+        });
+    }
+
+    // draw texture on canvas
+    drawTexture(ctx, texturesName, x, y, rotate = 0) {
+        const texture = this.textureMap[texturesName] || null;
+        if (texture) {
+            ctx.save();
+            ctx.translate(x, y);
+            ctx.rotate((texture.rotate + rotate)* Math.PI/180);
+            ctx.drawImage(texture.image,
+                texture.x, texture.y, texture.width, texture.height,
+                -texture.anchor.x, -texture.anchor.y, texture.width, texture.height);
+            ctx.restore();
+        }
+    }
+
+    // load image files
+    async loadImages(imagePaths) {
+        return new Promise(resolve => {
+            const imagePromises = imagePaths.map(imagePath => this.loadImage(imagePath));
+            Promise.all(imagePromises)
+                .then(results => resolve(results));
+        });
+    }
+
+    // loading single image file
+    async loadImage(imagePath) {
+        return new Promise(resolve => {
+            const image = new Image();
+            image.addEventListener('load', () => resolve(image));
+            image.src = imagePath;
+        });
+    }
+
+    // draw frame which calls user-update handler function
+    lastMilliSec = null;
+    drawFrame(currentTime) {
+        const currentMilliSec = currentTime/1000;
+        const delta = this.lastMilliSec ? currentMilliSec - this.lastMilliSec : 0;
+        const ctx = (this.offScreenCtx ? this.offScreenCtx : this.ctx);
+        this.lastMilliSec = currentMilliSec;
+        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.updateHandler(this, delta);
+        // draw sprites
+        const spriteLayerNos = Object.keys(this.sprites);
+        spriteLayerNos.forEach(layerNo => {
+            const spriteTags = Object.keys(this.sprites[layerNo]);
+            spriteTags.forEach(spriteTag => this.sprites[layerNo][spriteTag].draw());
+        });
+        if (this.displayFps) {
+            const fps = (1/delta).toFixed(1);
+            ctx.save();
+            ctx.fillStyle = 'black';
+            ctx.font = '24px';
+            ctx.fillText(fps+' fps', 10, 10);
+            ctx.restore();
+        }
+        if (this.offScreenCanvas) {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.ctx.drawImage(this.offScreenCanvas,
+                0, 0, this.canvas.width, this.canvas.height,
+                0, 0, this.canvas.width, this.canvas.height);
+        }
+        requestAnimationFrame(this.drawFrame.bind(this));
+    }
+};
+
+export class Sprite {
+    x = 0;
+    y = 0;
+    rotate = 0;
+    name = null;
+    layerNo = 0;
+    tag = null;
+
+    /**
+     * constructor
+     * @param engine frame engine
+     * @param name texture name
+     * @param layerNo drawing layer no
+     * @param tag tag name
+     */
+    constructor(engine, name, layerNo, tag) {
+        this.engine = engine;
+        this.name = name;
+        this.layerNo = layerNo;
+        this.tag = tag;
+    }
+    // set sprite's position (and rotation)
+    setPosition(x, y, rotate = 0) {
+        [this.x, this.y, this.rotate] = [x, y, rotate];
+    }
+    // draw sprite
+    draw = () => this.engine.drawTexture(this.engine.getCtx(), this.name, this.x, this.y, this.rotate);
+}
+
+class Particle { // divide into base and concrete class
+    cx = 0;
+    cy = 0;
+    textureName = null;
+    options = {
+        duration: 1, // sec
+        initialVelocity: 10, // px/sec
+        radius: 50, // px
+        initialAlpha: 1,
+        numTextures: 30,
+        angularVelocity: 0, // deg/sec
+        initialVx: 0, // px/sec
+        initialVy: 0, // px/sec
+    };
+    textures = [];
+    // initialize own parameters
+    constructor(cx, cy, textureName, options={}) {
+        // set specified option values
+        Object.keys(this.options).forEach(key => {
+            this.options[key] = (options[key] ? options[key] : this.options[key]);
+        });
+        [this.cx, this.cy, this.textureName] = [cx, cy, textureName];
+
+        for (let i = 0; i < this.options.numTextures; i++) {
+            const texture = new ParticleTexture(); // TODO: be vary particle textures
+            [texture.x, texture.y] = [this.cx, this.cy];
+            const initialVx = this.options.initialVx * .5 + this.options.initialVx * .5 * Math.random(); // TODO: parameterize fixed velocity ratio
+            const initialVy = this.options.initialVy * .5 + this.options.initialVy * .5 * Math.random(); // TODO: parameterize fixed velocity ratio
+            [texture.initialVx, texture.initialVy] = [initialVx, initialVy];
+            this.textures.push(texture);
+        }
+    }
+    // update textures
+    update(engine, delta) {
+        this.textures.forEach(texture => texture.updateCoordinate(delta));
+        this.textures.forEach(texture => texture.draw(engine));
+    }
+}
+
+class ParticleTexture { // divide into base and concrete class
+    elapsedTime = 0;
+    x = 0;
+    y = 0;
+    initialVx = 0; // px/sec
+    initialVy = 0; // px/sec
+    vx = 0; // px/sec
+    vy = 0; // px/sec
+    rotate = 0;
+    vr = 0; // deg/sec
+    textureName = null;
+    updateElapsedTime(delta) {
+        this.elapsedTime += delta;
+    }
+    updateCoordinate(delta) {
+        super.updateElapsedTime(delta);
+        const velocityMultiplyValue = 1/(1 - Math.pow(Math.E, this.elapsedTime)); // TODO: may need adjust
+        this.vx = this.initialVx * velocityMultiplyValue;
+        this.vy = this.initialVy * velocityMultiplyValue;
+        this.x += (this.vx * delta);
+        this.y += (this.vy * delta);
+        this.rotate += (this.vr * delta);
+    }
+    draw(engine) {
+        const ctx = engine.getCtx();
+        engine.drawTexture(ctx, this.textureName, this.x, this.y, this.rotate); // TODO: to be using raw interface to get rendering speed up
+    }
+}
---
+++ particleSystem/main.js
@@ -0,0 +1,95 @@
+import FrameEngine, {Sprite} from "./FrameEngine.js";
+import atlas from './atlas.js';
+
+(function(){
+    // create engines on each canvas
+    [1, 2].forEach(index => {
+        const engine = new FrameEngine();
+        engine.setCanvas('canvas_'+index, (index === 1));
+        engine.setDisplayFps(true);
+        engine.setClientData({
+            pps: 20,
+            arrowKeyDownStatuses: {ArrowLeft: false, ArrowRight: false, ArrowUp: false, ArrowDown: false},
+            spaceKeyDownStatus: false
+        });
+
+        // create sprites
+        for (let col = 100; col < 500; col += 50) {
+            for (let row = 100; row < 500; row += 50) {
+                const sprite = new Sprite(engine, 'pengo_2.png', 0, `sprite_${row}_${col}`);
+                sprite.setPosition(row, col);
+                engine.addSprite(sprite);
+            }
+        }
+
+        // register event handlers
+        engine.setUpdateHandler(update);
+        engine.setEventHandler(engine.EVENT_TYPE_KEYDOWN, procKeyEvents);
+        engine.setEventHandler(engine.EVENT_TYPE_KEYUP, procKeyEvents);
+        engine.setEventHandler(engine.EVENT_TYPE_CLICK, procMouseClick);
+        // create sprites
+        engine.loadTextureAtlas(atlas)
+            .then(() => engine.startFrame());
+    });
+
+    // update canvas
+    function update(engine, delta) {
+        const canvas = engine.getCanvas();
+        const data = engine.getClientData();
+        for (let col = 100; col < 500; col += 50) {
+            for (let row = 100; row < 500; row += 50) {
+                const sprite = engine.getSprite(`sprite_${row}_${col}`);
+                if (sprite) {
+                    let x = sprite.x;
+                    let y = sprite.y;
+                    const pps = data.pps;
+
+                    // move sprite according to keydown status of arrow keys.
+                    const pxDelta = delta * pps;
+                    if (data.arrowKeyDownStatuses[engine.KEY_SYMBOL_LEFT]) {
+                        x = (x - pxDelta) % canvas.width;
+                    }
+                    if (data.arrowKeyDownStatuses[engine.KEY_SYMBOL_RIGHT]) {
+                        x = (x + pxDelta) % canvas.width;
+                    }
+                    if (data.arrowKeyDownStatuses[engine.KEY_SYMBOL_UP]) {
+                        y = (y - pxDelta) % canvas.height;
+                    }
+                    if (data.arrowKeyDownStatuses[engine.KEY_SYMBOL_DOWN]) {
+                        y = (y + pxDelta) % canvas.height;
+                    }
+                    const rotate = sprite.rotate + delta * 45;
+                    sprite.setPosition(x, y, rotate);
+                }
+            }
+        }
+    }
+
+    // process key events
+    function procKeyEvents(engine, e, eventType) {
+        const KEY_ARROW_KEYS = [engine.KEY_SYMBOL_LEFT, engine.KEY_SYMBOL_RIGHT, engine.KEY_SYMBOL_UP, engine.KEY_SYMBOL_DOWN];
+        const data = engine.getClientData();
+        const eventKey = e.key;
+        // check arrow keys
+        if (KEY_ARROW_KEYS.indexOf(eventKey) >= 0) {
+            data.arrowKeyDownStatuses[eventKey] = (eventType === engine.EVENT_TYPE_KEYDOWN);
+        }
+        // check space key
+        if (eventKey === engine.KEY_SYMBOL_SPACE) {
+            data.spaceKeyDownStatus = (eventType === engine.EVENT_TYPE_KEYDOWN);
+        }
+        engine.setClientData(data);
+    }
+
+    // process mouse click
+    function procMouseClick(engine, e) {
+        const rect = e.target.getBoundingClientRect();
+        const sprite = engine.getSprite('sprite_100_100');
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        sprite.setPosition(x, y, sprite.rotate);
+        if (engine.getSprite('sprite_150_100')) {
+            engine.removeSprite('sprite_150_100');
+        }
+    }
+})();
