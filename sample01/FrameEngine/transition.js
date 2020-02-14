57323d5455284be9256b45fe816cf3a60a6f8466
flipfrog <>
2020-02-14 17:46:25 +0900

    simplified method declarations.

--- sample01/FrameEngine/transition.js
+++ sample01/FrameEngine/transition.js
@@ -1,7 +1,9 @@
 export class TransitionBase {
     constructor() {
         this.t = 0;
-        this.draw = () => true;
+    }
+    draw () {
+        return true;
     }
 }
 
@@ -9,25 +11,25 @@ export class TransitionSwipe extends TransitionBase {
     constructor(wipeSpeed = 2000) {
         super();
         this.wipeSpeed = wipeSpeed;
-        // wipe out current scene while wipe in next scene
-        this.draw = (engine, delta) => {
-            this.t += delta;
-            if (this.t * this.wipeSpeed > engine.getCanvas().width) {
-                return true;
-            }
-            const ctx = engine.getCtx();
-            ctx.save();
-            const xOffset = this.t * this.wipeSpeed;
-            const currentScene = engine.scenes[engine.currentSceneIndex];
-            ctx.translate(xOffset, 0);
-            currentScene.updateHandler(engine, currentScene, delta);
-            currentScene.draw(this, delta);
-            const nextScene = engine.scenes[engine.requestedSceneIndex];
-            ctx.translate(-(engine.getCanvas().width - xOffset) - xOffset, 0);
-            nextScene.updateHandler(engine, nextScene, delta);
-            nextScene.draw(this, delta);
-            ctx.restore();
-            return false;
+    }
+    // wipe out current scene while wipe in next scene
+    draw (engine, delta) {
+        this.t += delta;
+        if (this.t * this.wipeSpeed > engine.getCanvas().width) {
+            return true;
         }
+        const ctx = engine.getCtx();
+        ctx.save();
+        const xOffset = this.t * this.wipeSpeed;
+        const currentScene = engine.scenes[engine.currentSceneIndex];
+        ctx.translate(xOffset, 0);
+        currentScene.updateHandler(engine, currentScene, delta);
+        currentScene.draw(this, delta);
+        const nextScene = engine.scenes[engine.requestedSceneIndex];
+        ctx.translate(-(engine.getCanvas().width - xOffset) - xOffset, 0);
+        nextScene.updateHandler(engine, nextScene, delta);
+        nextScene.draw(this, delta);
+        ctx.restore();
+        return false;
     }
 }
--- sample01/FrameEngine/ui.js
+++ sample01/FrameEngine/ui.js
@@ -10,9 +10,9 @@ export class UIBase {
         this.geometry.cx = cx;
         this.geometry.cy = cy;
         this.userEventListener = () => {};
-        this.computeGeometry = () => {};
-        this.draw = () => {};
     }
+    computeGeometry () {}
+    draw () {}
     eventListener(engine, scene, e) {
         if (e.type === engine.EVENT_TYPE_MOUSEDOWN) {
             const rect = e.target.getBoundingClientRect();
@@ -48,45 +48,45 @@ export class UIButton extends UIBase {
         this.engine = engine;
         this.text = text;
         this.shouldComputeGeometry = false;
-        this.computeGeometry = () => {
-            // TODO: add image and text rendering
-            if (this.imageName && !this.text) {
-                const texture = this.engine.textureMap[this.imageName];
-                this.geometry.width = texture.width;
-                this.geometry.height = texture.height;
-            } else {
-                this.ctx.save();
-                this.ctx.font = this.font;
-                this.textSize = this.ctx.measureText(this.text);
-                this.ctx.restore();
-                this.geometry.width = this.textSize.width;
-                this.geometry.height = this.textSize.actualBoundingBoxAscent + this.textSize.actualBoundingBoxDescent;
-            }
-            this.geometry.x = this.geometry.cx - this.geometry.width/2;
-            this.geometry.y = this.geometry.cy - this.geometry.height/2;
-        };
         this.computeGeometry();
-        this.draw = () => {
-            if (this.shouldComputeGeometry) {
-                this.computeGeometry();
-                this.shouldComputeGeometry = false;
-            }
+    }
+    computeGeometry () {
+        // TODO: add image and text rendering
+        if (this.imageName && !this.text) {
+            const texture = this.engine.textureMap[this.imageName];
+            this.geometry.width = texture.width;
+            this.geometry.height = texture.height;
+        } else {
             this.ctx.save();
-            // TODO: add image and text rendering
-            if (this.imageName && !this.text) {
-                this.engine.drawTexture(this.engine.getCtx(), this.imageName, this.geometry.cx, this.geometry.cy);
-                this.ctx.strokeRect(this.geometry.x, this.geometry.y, this.geometry.width, this.geometry.height);
-            } else {
-                if (this.font) {
-                    this.ctx.font = this.font;
-                }
-                this.ctx.fillStyle = this.rectFillStyle;
-                this.ctx.fillRect(this.geometry.x, this.geometry.y, this.geometry.width, this.geometry.height);
-                this.ctx.fillStyle = this.textFillStyle;
-                this.ctx.fillText(this.text, this.geometry.x, this.geometry.y + this.textSize.actualBoundingBoxAscent);
-                this.ctx.restore();
+            this.ctx.font = this.font;
+            this.textSize = this.ctx.measureText(this.text);
+            this.ctx.restore();
+            this.geometry.width = this.textSize.width;
+            this.geometry.height = this.textSize.actualBoundingBoxAscent + this.textSize.actualBoundingBoxDescent;
+        }
+        this.geometry.x = this.geometry.cx - this.geometry.width/2;
+        this.geometry.y = this.geometry.cy - this.geometry.height/2;
+    }
+    draw () {
+        if (this.shouldComputeGeometry) {
+            this.computeGeometry();
+            this.shouldComputeGeometry = false;
+        }
+        this.ctx.save();
+        // TODO: add image and text rendering
+        if (this.imageName && !this.text) {
+            this.engine.drawTexture(this.engine.getCtx(), this.imageName, this.geometry.cx, this.geometry.cy);
+            this.ctx.strokeRect(this.geometry.x, this.geometry.y, this.geometry.width, this.geometry.height);
+        } else {
+            if (this.font) {
+                this.ctx.font = this.font;
             }
-        };
+            this.ctx.fillStyle = this.rectFillStyle;
+            this.ctx.fillRect(this.geometry.x, this.geometry.y, this.geometry.width, this.geometry.height);
+            this.ctx.fillStyle = this.textFillStyle;
+            this.ctx.fillText(this.text, this.geometry.x, this.geometry.y + this.textSize.actualBoundingBoxAscent);
+            this.ctx.restore();
+        }
     }
     setText(text) {
         this.text = text;
